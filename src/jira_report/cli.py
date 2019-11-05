6bcfcc40b3b00081a9f10c01a6914fda7330cca6
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2019-11-05 11:54:09 +0100

    Estimate the number of hours worked based on story points

--- src/jira_report/cli.py
+++ src/jira_report/cli.py
@@ -19,11 +19,12 @@ import calendar
 import datetime
 import logging
 import os
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import dateutil.parser
 import environs
 import jira
+import workdays
 import xlwt
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
@@ -50,7 +51,7 @@ def main(args: argparse.Namespace) -> None:
         issues = find_issues(args.date, jira_config())
         if len(issues) > 0:
             LOGGER.info('Found %d tasks assigned to you during that period.', len(issues))
-            xls_export(issues, title, filename)
+            xls_export(issues, month_hours(args.date, args.business_days), title, filename)
         else:
             LOGGER.info('There were no tasks assigned to you during that period.')
 
@@ -64,6 +65,7 @@ def parse_args() -> argparse.Namespace:
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--force-overwrite', action='store_true', default=False)
+    parser.add_argument('-d', '--days', dest='business_days', type=int)
     parser.add_argument('--month',
                         metavar='YYYY/MM',
                         dest='date',
@@ -120,7 +122,21 @@ def month_days(date: datetime.date) -> int:
     return num_days
 
 
+def month_hours(date: datetime.date, business_days: Optional[int]) -> int:
+    """Return the number of work hours in the given month."""
+
+    if business_days is None:
+        start_date = datetime.date(date.year, date.month, 1)
+        end_date = datetime.date(date.year, date.month, month_days(date))
+        business_days = workdays.networkdays(start_date, end_date) * 8
+
+    LOGGER.info('Business days=%d (%d hours)', business_days, business_days * 8)
+
+    return business_days * 8
+
+
 def xls_export(issues: List[jira.Issue],
+               month_hours: int,
                title: str,
                filename: str) -> None:
     """Save Jira issues to a spreadsheet file."""
@@ -134,16 +150,16 @@ def xls_export(issues: List[jira.Issue],
         'Task ID',
         'Task Key',
         'Task URL',
-        'Created At',
-        'Created By',
         'Project Name',
-        'Status',
+        'Created By',
+        'Created At',
         'Description',
+        'Worklog',
     )
 
     bold = xlwt.easyxf('font: bold on; align: vert centre')
     for column, header in enumerate(column_headers):
-        sheet.write(0, column, header, bold)
+        write(sheet, 0, column, header, bold)
         sheet.row(0).height = row_height
 
     middle = xlwt.easyxf('align: vert centre')
@@ -151,16 +167,24 @@ def xls_export(issues: List[jira.Issue],
     date_format = xlwt.easyxf('align: vert centre, horiz left')
     date_format.num_format_str = 'yyyy-mm-dd, HH:MM'
 
+    hours_format = xlwt.easyxf('align: vert centre, horiz right')
+    hours_format.num_format_str = '#,#0.0 "h"'
+
+    invisible = xlwt.easyxf('align: vert centre; font: color white')
+
     for row, issue in enumerate(issues, 1):
         sheet.row(row).height = row_height
         write(sheet, row, 0, issue.id, middle)
         write(sheet, row, 1, issue.key, middle)
         write(sheet, row, 2, make_link(issue.permalink()), middle)
-        write(sheet, row, 3, make_datetime(issue.fields.created), date_format)
+        write(sheet, row, 3, issue.fields.project.name, middle)
         write(sheet, row, 4, issue.fields.creator.displayName, middle)
-        write(sheet, row, 5, issue.fields.project.name, middle)
-        write(sheet, row, 6, issue.fields.status.name, middle)
-        write(sheet, row, 7, issue.fields.summary, middle)
+        write(sheet, row, 5, make_datetime(issue.fields.created), date_format)
+        write(sheet, row, 6, issue.fields.summary, middle)
+        sheet.write(row, 7, hours_worked(row, issues), hours_format)
+        write(sheet, row, 8, story_points(issue), invisible)
+
+    write(sheet, 0, 8, month_hours, invisible)
 
     workbook.save(filename)
     logging.info('Exported file: "%s"', os.path.join(os.getcwd(), filename))
@@ -172,7 +196,7 @@ def write(sheet: xlwt.Worksheet, row: int, col: int, value: Any, style: xlwt.XFS
     sheet.write(row, col, value, style)
 
     char_width = 256
-    text_width = len(str(value))*char_width
+    text_width = len(str(value)) * char_width
 
     column = sheet.col(col)
     if column.get_width() < text_width:
@@ -187,3 +211,13 @@ def make_datetime(text: str) -> datetime.datetime:
 def make_link(url: str) -> xlwt.Formula:
     """Return an interactive hyperlink formula."""
     return xlwt.Formula(f'HYPERLINK("{url}")')
+
+
+def story_points(issue: jira.Issue) -> Optional[float]:
+    """Return the number of story points of None."""
+    return issue.fields.customfield_10020
+
+
+def hours_worked(row: int, issues: List) -> xlwt.Formula:
+    """Return a math formula to calculate the number of hours worked on an issue."""
+    return xlwt.Formula(f'I{row + 1}/SUM(I2:I{len(issues) + 1})*I1')
